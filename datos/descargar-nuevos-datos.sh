a6e5eef78a414098769cd92d71534068f6b95327
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-05-23 14:52:30 -0500

    descargar los datos de fallecimientos

--- datos/descargar-nuevos-datos.sh
+++ datos/descargar-nuevos-datos.sh
@@ -1,5 +1,13 @@
 #! /bin/bash -x
-wget -N https://www.datosabiertos.gob.pe/sites/default/files/DATOSABIERTOS_SISCOVID.zip
 
+# descargar archivos
+wget -N https://www.datosabiertos.gob.pe/sites/default/files/DATOSABIERTOS_SISCOVID.zip
+wget -N https://www.datosabiertos.gob.pe/sites/default/files/FALLECIDOS_CDC.csv
+
+# Pre-proceso de los datos de casos
 unzip -p DATOSABIERTOS_SISCOVID.zip | iconv  -f ISO_8859-1 -t UTF-8 - > DATOSABIERTOS_SISCOVID-utf8.csv
 gzip -9f DATOSABIERTOS_SISCOVID-utf8.csv
+
+# Pre-proceso de los datos de fallecidos
+iconv -f ISO_8859-1 -t UTF-8 FALLECIDOS_CDC.csv > FALLECIDOS_CDC-utf8.csv
+gzip -9f FALLECIDOS_*.csv
