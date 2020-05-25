b0aa5800bcacd7c008a40376553f381a292e8926
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-05-25 18:52:49 -0500

    mode cleanup tweaks

--- actualizar-datos.sh
+++ actualizar-datos.sh
@@ -1,15 +1,15 @@
 #! /bin/bash -x
 
 # descargar archivos
-wget https://www.datosabiertos.gob.pe/sites/default/files/DATOSABIERTOS_SISCOVID.zip -O datos/DATOSABIERTOS_SISCOVID.zip 
-wget https://www.datosabiertos.gob.pe/sites/default/files/FALLECIDOS_CDC.csv -O datos/FALLECIDOS_CDC.csv
+wget https://www.datosabiertos.gob.pe/sites/default/files/DATOSABIERTOS_SISCOVID.zip -O datos/originales/DATOSABIERTOS_SISCOVID.zip 
+wget https://www.datosabiertos.gob.pe/sites/default/files/FALLECIDOS_CDC.csv -O datos/originales/FALLECIDOS_CDC.csv
 
 # Pre-proceso de los datos de casos
-unzip -p datos/DATOSABIERTOS_SISCOVID.zip | iconv  -f ISO_8859-1 -t UTF-8 - > datos/DATOSABIERTOS_SISCOVID-utf8.csv
+unzip -p datos/originales/DATOSABIERTOS_SISCOVID.zip | iconv  -f ISO_8859-1 -t UTF-8 - > datos/DATOSABIERTOS_SISCOVID-utf8.csv
 #gzip -9f datos/DATOSABIERTOS_SISCOVID-utf8.csv
 
 # Pre-proceso de los datos de fallecidos
-iconv -f ISO_8859-1 -t UTF-8 datos/FALLECIDOS_CDC.csv > datos/FALLECIDOS_CDC-utf8.csv
+iconv -f ISO_8859-1 -t UTF-8 datos/originales/FALLECIDOS_CDC.csv > datos/FALLECIDOS_CDC-utf8.csv
 #gzip -9f datos/FALLECIDOS_*.csv
 
 # Limpieza inicial
